df724e94945efe2ceccdf336e375b5a75273f90c
Olivia <olivia.famjespersen@gmail.com>
2025-02-21 11:14:08 +0100

    Fix typo in variable name from 'recive' to 'receive'

--- test/test.c
+++ test/test.c
@@ -6,13 +6,13 @@
 
 int main(int argc, char **argv) {
     char *test = "Hello world!";
-    char recive[50];
+    char receive[50];
 
     syscall(__NR_dm510_msgbox_put, test, strlen(test)+1);
 
     syscall(__NR_dm510_msgbox_get, recive, 50);
 
-    printf("%s\n", recive);
+    printf("%s\n", receive);
 
     return 0;
 }
