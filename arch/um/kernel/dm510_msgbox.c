9a432c9564abfc65b1f4c7a2c19876ce24902790
Marcus Søndergaard <marcusodense@gmail.com>
2025-03-03 11:26:05 +0100

    Cleaned up comments

--- arch/um/kernel/dm510_msgbox.c
+++ arch/um/kernel/dm510_msgbox.c
@@ -9,70 +9,75 @@ struct _msg_t{
   char* message;
 };
 
-/* The stack is initialized as empty */
+// The stack is initialized as empty.
 static msg_t *top = NULL;
 
 int sys_dm510_msgbox_put( char *buffer, int length ) {
-  /* Gives an invalid argument error if the message length is invalid */
+  // Gives an invalid argument error if the message length is invalid.
   if (length < 0) { return -EINVAL; }
-  /* Gives a bad address error if the buffer isn't safe to write to */
+  // Gives a bad address error if the buffer isn't safe to write to.
   if (!access_ok(buffer, length)) { return -EFAULT; }
 
-  msg_t* msg = kmalloc(sizeof(msg_t), GFP_KERNEL); /* Memory is allocated in kernel space instead of user space */
-  if (msg == NULL) { return -ENOMEM; } /* If we get this error we assume that it's because we are out of memory */
+  // Memory is allocated in kernel space instead of user space.
+  msg_t* msg = kmalloc(sizeof(msg_t), GFP_KERNEL); 
+  // If we get this error we assume that it's because we are out of memory.
+  if (msg == NULL) { return -ENOMEM; } 
   msg->previous = NULL;
   msg->length = length;
   msg->message = kmalloc(length, GFP_KERNEL);
 
-  /* Free the memory we allocated in the first kmalloc if we fail to allocate memory for the message and give an out of memory error */
-  if (msg->message == NULL) { kfree(msg); return -ENOMEM; } /* If we get this error we assume that it's because of no mem. We also free the mem we allocated in the first kmalloc */
-  /* Copies the message from the user space to the kernel space and gives a bad address error if it fails */
+  // Free the memory we already allocated with kmalloc if we fail to allocate
+  // memory for the message. Return with an out of memory error.
+  if (msg->message == NULL) { kfree(msg); return -ENOMEM; }
+  // Copy the message from user to kernel space and give a bad address error on failure.
   if (copy_from_user(msg->message, buffer, length)) { return -EFAULT; }
 
-  /* Disables interrupts */
+  // Disable interrupts.
   unsigned long flags;
   local_irq_save(flags);
 
+  // Check if stack is empty.
   if (top == NULL) {
     top = msg;
   } else {
-    /* not empty stack */
     msg->previous = top;
     top = msg;
   }
 
-  /* Enables interrupts */
+  // Enable interrupts.
   local_irq_restore(flags);
 
   return 0;
 }
 
 int sys_dm510_msgbox_get( char* buffer, int length ) {
-  /* Gives an invalid argument error if the message length is invalid */
+  // Give an invalid argument error if the message length is invalid.
   if (length < 0) { return -EINVAL; }
-  /* Gives a bad address error if the buffer isn't safe to write to */
+  // Give a bad address error if the buffer isn't safe to write to.
   if (!access_ok(buffer, length)) { return -EFAULT; }
-  /* Gives a no message error if the stack is empty */
+  // Give a no message error if the stack is empty.
   if (top == NULL) { return -ENOMSG; }
 
-  /* Disables interrupts */
+  // Disable interrupts.
   unsigned long flags;
   local_irq_save(flags);
 
+  // Take the top message from the stack.
   msg_t* msg = top;
   top = msg->previous;
 
-  /* Enables interrupts */
+  // Enable interrupts.
   local_irq_restore(flags);
 
   int mlength = msg->length;
-  /* Gives a message size error if the message length is greater than the buffer length */
-  if (length < mlength) { return -EMSGSIZE; } /* We could also have an approach where we copy as much as possible */
+  // Give a message size error if the message length is greater than the buffer length.
+  // Alternatively, we could copy as much as possible.
+  if (length < mlength) { return -EMSGSIZE; }
 
-  /* Copies the message from the kernel space to the user space and gives a bad address error if it fails */
+  // Copy the message from kernel to user space and give a bad address error on failure.
   if (copy_to_user(buffer, msg->message, mlength)) { return -EFAULT; }
 
-  /* free memory */
+  // Free memory.
   kfree(msg->message);
   kfree(msg);
 
